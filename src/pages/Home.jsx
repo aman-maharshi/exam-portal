db284ee7d5ae614b8b11400003866a54d78ef763
aman-maharshi <amanm4harshi@gmail.com>
2025-03-01 04:53:36 +0530

    feat: tests filter tabs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import Sidebar from '../components/Sidebar'
 import AvailableTestRow from '../components/AvailableTestRow'
 import Topbar from '../components/Topbar'
 import InfoCard from '../components/InfoCard'
+import clsx from 'clsx'
 
 const Home = () => {
   const { userData, setUserData } = useContext(GlobalContext)
@@ -20,6 +21,9 @@ const Home = () => {
 
   const filteredData = data.filter(item => item.class === userData?.grade)
   const [availableTests, setAvailableTests] = useState(filteredData)
+  const [availabeTestsCopy, setAvailabeTestsCopy] = useState(filteredData)
+  const [activeTab, setActiveTab] = useState('All')
+  const [showFilterTabs, setShowFilterTabs] = useState(false)
   // console.log(availableTests, "availableTests")
 
   useEffect(() => {
@@ -31,6 +35,26 @@ const Home = () => {
       setAvailableTests([...remainingTests, ...attemptedTests])
     }
   }, [userData])
+
+  useEffect(() => {
+    const hasNonEasyTests = availableTests.some(test => test.difficulty !== 'Easy')
+    setShowFilterTabs(hasNonEasyTests)
+  }, [availableTests])
+
+  const handleFilter = (tab) => {
+    if (tab === 'All') {
+      setAvailableTests(availabeTestsCopy)
+      setActiveTab('All')
+    } 
+    if (tab === 'Easy') {
+      setAvailableTests([...availabeTestsCopy].filter(test => test.difficulty === 'Easy'))
+      setActiveTab('Easy')
+    }
+    if (tab === 'Moderate') {
+      setAvailableTests([...availabeTestsCopy].filter(test => test.difficulty === 'Moderate'))
+      setActiveTab('Moderate')
+    }
+  }
 
   return (
     <Layout>
@@ -46,7 +70,40 @@ const Home = () => {
               image="/study-male.svg"
             />
             <div className='mt-6'>
-              <h3 className='text-xl font-bold'>Available Tests</h3>
+              <div className='flex gap-6'>
+                <h3 className='text-xl font-bold'>Available Tests</h3>
+                <div className='flex space-x-2'>
+                  <button
+                    className={clsx(
+                      'px-4 py-1 text-sm bg-transparent rounded-3xl',
+                      activeTab === 'All' ? 'cta-gradient text-white' : 'text-black'
+                    )}
+                    onClick={() => handleFilter('All')}
+                  >
+                    All
+                  </button>
+                  <button
+                    className={clsx(
+                      'px-4 py-1 text-sm bg-transparent rounded-3xl',
+                      activeTab === 'Easy' ? 'cta-gradient text-white' : 'text-black'
+                    )}
+                    onClick={() => handleFilter('Easy')}
+                  >
+                    Easy
+                  </button>
+                  <button
+                    className={clsx(
+                      'px-4 py-1 text-sm bg-transparent rounded-3xl',
+                      activeTab === 'Moderate' ? 'cta-gradient text-white' : 'text-black'
+                    )}
+                    onClick={() => handleFilter('Moderate')}
+                  >
+                    Medium
+                  </button>
+                </div>
+              </div>
+
+
 
               {availableTests.map((test, index) => (
                 <AvailableTestRow
