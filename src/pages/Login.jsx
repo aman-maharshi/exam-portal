7786356ffb7622e02014fcd74c64fb1a9c5add7d
aman-maharshi <amanm4harshi@gmail.com>
2025-08-01 20:30:27 +0530

    login form fixes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react"
 import GlobalContext from "../GlobalContext"
-import { useNavigate } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom"
+import { ToastContainer, toast } from "react-toastify"
 import { defaultPassword } from "../utils/apiConstants"
-import clsx from 'clsx'
+import clsx from "clsx"
 
 // ICONS
 import DownArrow from "../assets/down-arrow.svg?react"
@@ -13,87 +13,98 @@ import HidePassword from "../assets/password-hide.svg?react"
 
 const Login = () => {
   const { userData, setUserData } = useContext(GlobalContext)
-  const [username, setUsername] = useState('')
-  const [grade, setGrade] = useState('')
-  const [password, setPassword] = useState('')
-  const [role, setRole] = useState('student')
+  const [username, setUsername] = useState("")
+  const [grade, setGrade] = useState("")
+  const [password, setPassword] = useState("")
+  const [role, setRole] = useState("student")
   const [showPassword, setShowPassword] = useState(false)
   const navigate = useNavigate()
   const testUserPassword = import.meta.env.VITE_DEFAULT_PASSWORD
 
-  const handleLogin = () => {
-    if (role !== 'student') {
-      return showToast('Unauthorized')
+  const handleLogin = e => {
+    e?.preventDefault()
+
+    if (role !== "student") {
+      return showToast("Unauthorized")
     }
 
     if (!username || !grade || !password) {
-      return showToast('All fields are required')
+      return showToast("All fields are required")
     }
 
     if (password !== defaultPassword) {
-      return showToast('Incorrect Password!')
+      return showToast("Incorrect Password!")
     }
 
     setUserData({ username, password, grade, role, loggedIn: true, results: [] })
-    navigate('/home')
+    navigate("/home")
   }
 
-  const showToast = (message) => {
+  const showToast = message => {
     toast(message, {
       position: "top-center",
       type: "error",
       limit: 1,
-      autoClose: 5000,
+      autoClose: 5000
     })
   }
 
   return (
-    <div className='w-full min-h-screen bg-gradient text-[#1b1b1b]'>
-      <div className='min-h-screen flex flex-col gap-10 items-center'>
-        <div className='bg-white p-10 rounded-xl card-shadow flex flex-col w-full sm:w-[450px] mt-24'>
-          <div className='flex justify-center mb-6'>
+    <div className="w-full min-h-screen bg-gradient text-[#1b1b1b]">
+      <div className="min-h-screen flex flex-col gap-10 items-center">
+        <form
+          onSubmit={handleLogin}
+          className="bg-white p-10 rounded-xl card-shadow flex flex-col w-full sm:w-[450px] mt-24"
+        >
+          <div className="flex justify-center mb-6">
             <button
+              type="button"
               className={clsx(
-                'py-2 px-4 rounded-l-lg transition-all font-medium duration-300 outline-none',
-                role === 'student' ? 'cta-gradient text-white' : 'bg-gray-200 text-black'
+                "py-2 px-4 rounded-l-lg transition-all font-medium duration-300 outline-none",
+                role === "student" ? "cta-gradient text-white" : "bg-gray-200 text-black"
               )}
-              onClick={() => setRole('student')}
+              onClick={() => setRole("student")}
             >
               Student Login
             </button>
             <button
+              type="button"
               className={clsx(
-                'py-2 px-4 rounded-r-lg transition-all font-medium duration-300 outline-none',
-                role === 'teacher' ? 'cta-gradient text-white' : 'bg-gray-200 text-black'
+                "py-2 px-4 rounded-r-lg transition-all font-medium duration-300 outline-none",
+                role === "teacher" ? "cta-gradient text-white" : "bg-gray-200 text-black"
               )}
-              onClick={() => setRole('teacher')}
+              onClick={() => setRole("teacher")}
             >
               Teacher Login
             </button>
           </div>
 
-          <h2 className='text-3xl font-bold text-center mb-2 mt-4'>Exam Portal</h2>
-          <p className='mb-6 text-center text-gray-500'>Please enter your details below to start your test</p>
+          <h2 className="text-3xl font-bold text-center mb-2 mt-4">Exam Portal</h2>
+          <p className="mb-6 text-center text-gray-500">Please enter your details below to start your test</p>
 
           <input
             type="text"
-            placeholder='Enter your Name'
-            className='border p-2 rounded-lg mb-4'
+            placeholder="Enter your Name"
+            className="border p-2 rounded-lg mb-4"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={e => setUsername(e.target.value)}
+            required
           />
 
-          {role === 'student' && (
-            <div className='border p-2 rounded-lg overflow-hidden mb-4 flex items-center justify-between'>
+          {role === "student" && (
+            <div className="border p-2 rounded-lg overflow-hidden mb-4 flex items-center justify-between">
               <select
                 className={clsx(
-                  'appearance-none outline-none flex-1 cursor-pointer bg-transparent',
-                  grade ? 'text-black' : 'text-gray-400'
+                  "appearance-none outline-none flex-1 cursor-pointer bg-transparent",
+                  grade ? "text-black" : "text-gray-400"
                 )}
                 value={grade}
-                onChange={(e) => setGrade(e.target.value)}
+                onChange={e => setGrade(e.target.value)}
+                required
               >
-                <option value="" disabled>Select your class</option>
+                <option value="" disabled>
+                  Select your class
+                </option>
                 <option value="7th">7th</option>
                 <option value="8th">8th</option>
                 <option value="9th">9th</option>
@@ -102,17 +113,18 @@ const Login = () => {
             </div>
           )}
 
-          <div className='relative mb-4'>
+          <div className="relative mb-4">
             <input
-              type={showPassword ? 'text' : 'password'}
-              placeholder='Enter Password'
-              className='border p-2 rounded-lg w-full pr-10'
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter Password"
+              className="border p-2 rounded-lg w-full pr-10"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={e => setPassword(e.target.value)}
+              required
             />
             <button
               type="button"
-              className='absolute right-2 top-1/2 -translate-y-1/2 text-sm text-gray-600'
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-gray-600"
               onClick={() => setShowPassword(!showPassword)}
             >
               {showPassword ? (
@@ -123,24 +135,19 @@ const Login = () => {
             </button>
           </div>
 
-          <button
-            onClick={handleLogin}
-            className='p-2 mt-4 rounded-lg text-white transition-all duration-300 cta-gradient'
-          >
+          <button type="submit" className="p-2 mt-4 rounded-lg text-white transition-all duration-300 cta-gradient">
             Login
           </button>
-        </div>
+        </form>
 
-        {role === 'student' && (
-          <div className='p-4 w-[250px] bg-stone-200 border border-stone-400 rounded-md flex items-center gap-6'>
+        {role === "student" && (
+          <div className="p-4 w-[250px] bg-stone-200 border border-stone-400 rounded-md flex items-center gap-6">
             <div>
               <UserInfo className="h-7 w-7 text-black" />
             </div>
             <div>
-              <div className='text-sm font-bold'>Demo Student Password</div>
-              <div className='text-sm text-stone-700'>
-                {testUserPassword}
-              </div>
+              <div className="text-sm font-bold">Demo Student Password</div>
+              <div className="text-sm text-stone-700">{testUserPassword}</div>
             </div>
           </div>
         )}
