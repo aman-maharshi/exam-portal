da93efa3677f1f0eb49dc49a7e6f6d1210b1ca8b
aman-maharshi <amanm4harshi@gmail.com>
2025-08-02 19:55:20 +0530

    minor change

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -10,7 +10,9 @@ const Topbar = ({ userData }) => {
 
   return (
     <div className="flex flex-row justify-between items-center pl-12 lg:pl-0">
-      <h1 className="font-bold text-lg sm:text-xl lg:text-3xl">Welcome, {userData?.username} 👋</h1>
+      <div>
+        <h1 className="hidden sm:block font-bold text-lg sm:text-xl lg:text-3xl">Welcome, {userData?.username} 👋</h1>
+      </div>
       <div className="flex items-center gap-4">
         <div className="text-base font-bold">{userData?.selectedClass} Class</div>
         <button
